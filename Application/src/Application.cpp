fc22a3b73d50d06deb352154f4e02f3437d9dbc0
TekMike365 <miskosklenka@gmail.com>
2024-06-29 10:12:22 +0200

    Added GLFW

--- Application/src/Application.cpp
+++ Application/src/Application.cpp
@@ -1,6 +1,8 @@
 #include "btpch.h"
 #include "Application.h"
 
+#include "GLFW/glfw3.h"
+
 namespace Botanica
 {
 
@@ -11,6 +13,43 @@ namespace Botanica
     void Application::Run()
     {
         BT_INFO("Hello, Botanica!");
+
+        GLFWwindow* window;
+
+        /* Initialize the library */
+        if (!glfwInit())
+        {
+            BT_ERROR("GLFW failed to initialize.");
+            return;
+        }
+
+        /* Create a windowed mode window and its OpenGL context */
+        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        if (!window)
+        {
+            BT_ERROR("GLFW failed to create window.");
+            glfwTerminate();
+            return;
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClearColor(0, 0.6, 0.9, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
     }
 
 }
