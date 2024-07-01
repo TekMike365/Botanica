b8f12c1302232be9379ee6228c14787c469ae4a0
TekMike365 <miskosklenka@gmail.com>
2024-07-01 13:10:14 +0200

    expanded opengl test

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,7 +8,7 @@ namespace Botanica
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
-    Application* Application::s_Instance = nullptr;
+    Application *Application::s_Instance = nullptr;
 
     Application::Application()
     {
@@ -26,12 +26,12 @@ namespace Botanica
         BT_INFO("Running application.");
 
         const char *vsSrc =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 inPosition;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = vec4(inPosition, 1.0);\n"
-        "}\n";
+            "#version 330 core\n"
+            "layout (location = 0) in vec3 inPosition;\n"
+            "void main()\n"
+            "{\n"
+            "    gl_Position = vec4(inPosition, 1.0);\n"
+            "}\n";
 
         const char *fsSrc =
             "#version 330 core\n"
@@ -45,21 +45,26 @@ namespace Botanica
 
         float vertices[] = {
             -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f};
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+        };
+
+        uint8_t indices[] = {
+            0, 1, 2
+        };
 
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         BT_ASSERT(status, "Glad it fucked up!");
 
         // create program object
-        unsigned int pId = glCreateProgram();
+        uint32_t pId = glCreateProgram();
 
         // create shaders
-        unsigned int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+        uint32_t vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertexShaderId, 1, &vsSrc, NULL);
         glCompileShader(vertexShaderId);
 
-        unsigned int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+        uint32_t fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragmentShaderId, 1, &fsSrc, NULL);
         glCompileShader(fragmentShaderId);
 
@@ -70,18 +75,21 @@ namespace Botanica
         glLinkProgram(pId);
 
         // buffer
-        unsigned int vertexBuffer;
-        unsigned int vertexArray;
-        glGenVertexArrays(1, &vertexArray);
-        glGenBuffers(1, &vertexBuffer);
+        uint32_t vao;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
 
-        glBindVertexArray(vertexArray);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        uint32_t ebo;
+        glGenBuffers(1, &ebo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+        uint32_t vbo;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
         glEnableVertexAttribArray(0);
 
         while (m_Running)
@@ -90,10 +98,8 @@ namespace Botanica
             glClear(GL_COLOR_BUFFER_BIT);
 
             glUseProgram(pId);
-
-            glBindVertexArray(vertexArray);
-
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glBindVertexArray(vao);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE, 0);
 
             m_Window->OnUpdate();
         }
@@ -101,7 +107,7 @@ namespace Botanica
 
     void Application::OnEvent(Event &e)
     {
-        //BT_TRACE("{}", e.ToString());
+        // BT_TRACE("{}", e.ToString());
 
         EventDispatcher dispatcher(e);
 
