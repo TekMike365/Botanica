ee23c6c44b6a20ea0b5cebc7f4e793881b1a1830
TekMike365 <miskosklenka@gmail.com>
2024-10-14 18:15:18 +0200

    cleanup

--- src/Core.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <signal.h>
-
-#define BT_ASSERT(x, ...) { if (!(x)) { BT_ERROR("Assertion failed: {}", __VA_ARGS__); raise(SIGTRAP); } }
-
-#define BIT(x) (1 << x)
--- src/Core/EntryPoint.cpp
+++ src/Core/EntryPoint.cpp
@@ -1,11 +1,3 @@
-#include "btpch.h"
-#include "Application.h"
-
 int main()
 {
-    Botanica::Log::Init(); // temp
-
-    Botanica::Application* app = new Botanica::Application();
-    app->Run();
-    delete app;
 }
