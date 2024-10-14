2dbcbbde32373868a352dbb8e265036801856978
TekMike365 <miskosklenka@gmail.com>
2024-10-14 18:22:27 +0200

    added precompiled headers

---
+++ src/Core/cpch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <memory>
+
+#include <functional>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
