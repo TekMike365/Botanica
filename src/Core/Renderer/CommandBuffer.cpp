758f6ad4575668fab46cfc86d3a866647f310893
TekMike365 <miskosklenka@gmail.com>
2024-11-23 16:15:03 +0100

    did some things

--- src/Core/Renderer/CommandBuffer.cpp
+++ src/Core/Renderer/CommandBuffer.cpp
@@ -6,14 +6,24 @@ namespace Botanica::Renderer
 {
     RendererAPI *CommandBuffer::s_Renderer = RendererAPI::Create();
 
-    void CommandBuffer::DrawIndexed(size_t count, size_t offset)
+    void CommandBuffer::SetShader(std::shared_ptr<Shader> shader)
     {
-        s_Renderer->DrawIndexed(count, offset);
+        shader->Bind();
     }
-    
+
+    void CommandBuffer::SetVertexArray(std::shared_ptr<VertexArray> va)
+    {
+        va->Bind();
+    }
+
     void CommandBuffer::SetClearColor(glm::vec4 color)
     {
         s_Renderer->SetClearColor(color);
+    }
+
+    void CommandBuffer::DrawIndexed(size_t count, size_t offset)
+    {
+        s_Renderer->DrawIndexed(count, offset);
     }
     
     void CommandBuffer::ClearScreen()
--- src/Core/Renderer/CommandBuffer.h
+++ src/Core/Renderer/CommandBuffer.h
@@ -2,6 +2,8 @@
 
 #include <glm/glm.hpp>
 
+#include "Shader.h"
+#include "VertexArray.h"
 #include "RendererAPI.h"
 
 namespace Botanica::Renderer
@@ -9,8 +11,11 @@ namespace Botanica::Renderer
     class CommandBuffer
     {
     public:
-        static void DrawIndexed(size_t count, size_t offset);
+        static void SetShader(std::shared_ptr<Shader> shader);
+        static void SetVertexArray(std::shared_ptr<VertexArray> va);
         static void SetClearColor(glm::vec4 color);
+
+        static void DrawIndexed(size_t count, size_t offset);
         static void ClearScreen();
 
     private:
