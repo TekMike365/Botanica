208c38f4c6a01893846bf92b6991b54179b422b6
TekMike365 <miskosklenka@gmail.com>
2024-12-08 12:11:19 +0100

    Application

---
+++ src/Application.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+#include "Application.h"
+
+#include "Log.h"
+
+Application::Application()
+{
+}
+
+Application::~Application()
+{
+}
+
+void Application::Run()
+{
+    BT_DLOG_INFO("Running application.");
+}
