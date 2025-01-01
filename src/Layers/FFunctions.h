e5fb75cb470cc5616fcd3562c45b93b7e06fdfab
Frantisek Knapec <knapecfrantisek@gmail.com>
2025-01-01 20:40:39 +0100

    terain generation

---
+++ src/Layers/FFunctions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <time.h>
+#include <stdlib.h>
+
+#ifndef FFUNCTIONS_H
+#define FFUNCTIONS_H
+
+void Generate()
+{
+    srand(time(NULL));
+
+}
+
+#endif
--- src/Layers/SimulationLayer.cpp
+++ src/Layers/SimulationLayer.cpp
@@ -1,4 +1,5 @@
 #include "SimulationLayer.h"
+#include "Platform.h"
 
 SimulationLayer::SimulationLayer(World &world)
     :m_World(world)
@@ -7,6 +8,36 @@ SimulationLayer::SimulationLayer(World &world)
 
 void SimulationLayer::OnAttach()
 {
+
+    std::vector<int> zValues;
+
+    srand(time(NULL));
+    std::vector<int> perlinNoiseRandomVector;
+    for (int i = 0; i < 4; i++)
+    {
+        perlinNoiseRandomVector.emplace_back(rand()%16);
+    }
+    double Pie = 3.14159265358979323846;
+
+    for (int x = 0; x < m_World.VoxelIDs.GetSize().x; x++)
+    {
+        for (int z = 0; z < m_World.VoxelIDs.GetSize().z; z++)
+        {
+            double protoHeight = 0;
+            std::array<double,4>circleConstants = {1/sqrt(pow(z+1,2)+pow(x+1,2)),1/sqrt(pow(z+1,2)+pow(m_World.VoxelIDs.GetSize().x-x+1,2)),1/sqrt(pow(x+1,2)+pow(m_World.VoxelIDs.GetSize().z-z+1,2)),1/sqrt(pow(m_World.VoxelIDs.GetSize().x-x+1,2)+pow(m_World.VoxelIDs.GetSize().z-z+1,2))};
+
+            for (int i = 0; i < 4; i++)
+            {
+                protoHeight += (sin(perlinNoiseRandomVector[i]/8*Pie)*z*circleConstants[i]+cos(perlinNoiseRandomVector[i]/8*Pie)*x*circleConstants[i])*15.5+15.5;
+            }
+            
+            for (int y = 0; y < (int)protoHeight/4; y++)
+            {
+                m_World.VoxelIDs[glm::uvec3(x,y,z)] = y%8+1;
+            }
+        }
+    }
+
 }
 
 void SimulationLayer::OnUpdate(Timestep dt)
