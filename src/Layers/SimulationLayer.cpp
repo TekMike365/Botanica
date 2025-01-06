080cfdad0c167be41abb003398989a3b3c20b3be
Frantisek Knapec <knapecfrantisek@gmail.com>
2025-01-06 22:54:02 +0100

    terrain generation done without water

--- src/Layers/SimulationLayer.cpp
+++ src/Layers/SimulationLayer.cpp
@@ -1,5 +1,11 @@
 #include "SimulationLayer.h"
 #include "Platform.h"
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <random>
 
 SimulationLayer::SimulationLayer(World &world)
     :m_World(world)
@@ -8,38 +14,67 @@ SimulationLayer::SimulationLayer(World &world)
 
 void SimulationLayer::OnAttach()
 {
+    auto sizeX = m_World.VoxelIDs.GetSize().x;
+    auto sizeY = m_World.VoxelIDs.GetSize().y;
+    auto sizeZ = m_World.VoxelIDs.GetSize().z;
 
-    std::vector<int> zValues;
-
+    std::vector<std::vector<int>> height(sizeX, std::vector<int>(sizeZ,0));
     srand(time(NULL));
     std::vector<int> perlinNoiseRandomVector;
+
     for (int i = 0; i < 4; i++)
     {
         perlinNoiseRandomVector.emplace_back(rand()%16);
     }
     double Pie = 3.14159265358979323846;
 
-    for (int x = 0; x < m_World.VoxelIDs.GetSize().x; x++)
-    {
-        for (int z = 0; z < m_World.VoxelIDs.GetSize().z; z++)
-        {
-            double protoHeight = 0;
-            std::array<double,4>circleConstants = {1/sqrt(pow(z+1,2)+pow(x+1,2)),1/sqrt(pow(z+1,2)+pow(m_World.VoxelIDs.GetSize().x-x+1,2)),1/sqrt(pow(x+1,2)+pow(m_World.VoxelIDs.GetSize().z-z+1,2)),1/sqrt(pow(m_World.VoxelIDs.GetSize().x-x+1,2)+pow(m_World.VoxelIDs.GetSize().z-z+1,2))};
+    for (int x = 0; x < sizeX; x++) {
+        for (int z = 0; z < sizeZ; z++) {
+            double finalHeight = 0.0;
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 4; i++) {
+                // Current corner vector
+                double cornerX, cornerZ;
+
+                switch (i) {
+                case 0: cornerX = x + 0.5; cornerZ = z + 0.5; break;
+                case 1: cornerX = sizeX - x - 0.5; cornerZ = z + 0.5; break;
+                case 2: cornerX = x + 0.5; cornerZ = sizeZ - z - 0.5; break;
+                case 3: cornerX = sizeX - x - 0.5; cornerZ = sizeZ - z - 0.5; break;
+                }
+
+                // Normalize the direction vector
+                double magnitude = sqrt(cornerX * cornerX + cornerZ * cornerZ);
+                double normX = cornerX / magnitude;
+                double normZ = cornerZ / magnitude;
+
+                // Get random vector from perlinNoiseRandomVector
+                double randomAngle = perlinNoiseRandomVector[i] / 16.0 * 2.0 * Pie;
+                double randX = cos(randomAngle);
+                double randZ = sin(randomAngle);
+
+                // Compute dot product
+                double dotProduct = normX * randX + normZ * randZ;
+
+                // Normalize to [0, 31]
+                double normalizedHeight = ((dotProduct + 1.0) / 2.0) * 31.0;
+
+                finalHeight += normalizedHeight;
+            }
+
+            // Average over 4 corners
+            height[x][z] = static_cast<int>(finalHeight / 4.0);
+
+            for (int y = 0; y < height[x][z]; y++)
             {
-                protoHeight += (sin(perlinNoiseRandomVector[i]/8*Pie)*z*circleConstants[i]+cos(perlinNoiseRandomVector[i]/8*Pie)*x*circleConstants[i])*15.5+15.5;
+                m_World.VoxelIDs[glm::vec3(x,y,z)] =y%8+1;
             }
             
-            for (int y = 0; y < (int)protoHeight/4; y++)
-            {
-                m_World.VoxelIDs[glm::uvec3(x,y,z)] = y%8+1;
-            }
         }
     }
-
 }
 
 void SimulationLayer::OnUpdate(Timestep dt)
 {
+
 }
