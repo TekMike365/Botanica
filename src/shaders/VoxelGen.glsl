2b56df2419ec451553cb76ab69930b4eaa5ae553
TekMike365 <miskosklenka@gmail.com>
2025-01-07 14:18:26 +0100

    shading

--- src/shaders/VoxelGen.glsl
+++ src/shaders/VoxelGen.glsl
@@ -48,14 +48,14 @@ void main()
         return;
 
     vec4 pos = vec4(GetGlobalPosition(idx), 0.0);
-    vec4 colorID = vec4(0.0, 0.0, 0.0, vID - 1);
+    vec4 colorID = vec4(0.0, 0.0, 0.0, (vID - 1) * 3);
     uint vertIdx = idx * 24;
 
     // bottom
-    ib_Vertices[vertIdx + 0]  = uVoxelScale * (g_Vertices[0] + pos) + colorID;
-    ib_Vertices[vertIdx + 1]  = uVoxelScale * (g_Vertices[1] + pos) + colorID;
-    ib_Vertices[vertIdx + 2]  = uVoxelScale * (g_Vertices[2] + pos) + colorID;
-    ib_Vertices[vertIdx + 3]  = uVoxelScale * (g_Vertices[3] + pos) + colorID;
+    ib_Vertices[vertIdx + 0]  = uVoxelScale * (g_Vertices[0] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
+    ib_Vertices[vertIdx + 1]  = uVoxelScale * (g_Vertices[1] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
+    ib_Vertices[vertIdx + 2]  = uVoxelScale * (g_Vertices[2] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
+    ib_Vertices[vertIdx + 3]  = uVoxelScale * (g_Vertices[3] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
 
     // top
     ib_Vertices[vertIdx + 4]  = uVoxelScale * (g_Vertices[4] + pos) + colorID;
@@ -64,16 +64,16 @@ void main()
     ib_Vertices[vertIdx + 7]  = uVoxelScale * (g_Vertices[7] + pos) + colorID;
 
     // front
-    ib_Vertices[vertIdx + 8]  = uVoxelScale * (g_Vertices[0] + pos) + colorID;
-    ib_Vertices[vertIdx + 9]  = uVoxelScale * (g_Vertices[1] + pos) + colorID;
-    ib_Vertices[vertIdx + 10] = uVoxelScale * (g_Vertices[5] + pos) + colorID;
-    ib_Vertices[vertIdx + 11] = uVoxelScale * (g_Vertices[4] + pos) + colorID;
+    ib_Vertices[vertIdx + 8]  = uVoxelScale * (g_Vertices[0] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
+    ib_Vertices[vertIdx + 9]  = uVoxelScale * (g_Vertices[1] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
+    ib_Vertices[vertIdx + 10] = uVoxelScale * (g_Vertices[5] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
+    ib_Vertices[vertIdx + 11] = uVoxelScale * (g_Vertices[4] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
 
     // back
-    ib_Vertices[vertIdx + 12] = uVoxelScale * (g_Vertices[2] + pos) + colorID;
-    ib_Vertices[vertIdx + 13] = uVoxelScale * (g_Vertices[3] + pos) + colorID;
-    ib_Vertices[vertIdx + 14] = uVoxelScale * (g_Vertices[7] + pos) + colorID;
-    ib_Vertices[vertIdx + 15] = uVoxelScale * (g_Vertices[6] + pos) + colorID;
+    ib_Vertices[vertIdx + 12] = uVoxelScale * (g_Vertices[2] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
+    ib_Vertices[vertIdx + 13] = uVoxelScale * (g_Vertices[3] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
+    ib_Vertices[vertIdx + 14] = uVoxelScale * (g_Vertices[7] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
+    ib_Vertices[vertIdx + 15] = uVoxelScale * (g_Vertices[6] + pos) + colorID + vec4(0.0, 0.0, 0.0, 2.0);
 
     // left
     ib_Vertices[vertIdx + 16] = uVoxelScale * (g_Vertices[0] + pos) + colorID;
@@ -82,8 +82,8 @@ void main()
     ib_Vertices[vertIdx + 19] = uVoxelScale * (g_Vertices[4] + pos) + colorID;
 
     // right
-    ib_Vertices[vertIdx + 20] = uVoxelScale * (g_Vertices[1] + pos) + colorID;
-    ib_Vertices[vertIdx + 21] = uVoxelScale * (g_Vertices[2] + pos) + colorID;
-    ib_Vertices[vertIdx + 22] = uVoxelScale * (g_Vertices[6] + pos) + colorID;
-    ib_Vertices[vertIdx + 23] = uVoxelScale * (g_Vertices[5] + pos) + colorID;
+    ib_Vertices[vertIdx + 20] = uVoxelScale * (g_Vertices[1] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
+    ib_Vertices[vertIdx + 21] = uVoxelScale * (g_Vertices[2] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
+    ib_Vertices[vertIdx + 22] = uVoxelScale * (g_Vertices[6] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
+    ib_Vertices[vertIdx + 23] = uVoxelScale * (g_Vertices[5] + pos) + colorID + vec4(0.0, 0.0, 0.0, 1.0);
 }
