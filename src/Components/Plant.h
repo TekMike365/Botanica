2c8ab98937b79f8faa9dca78a37fd665f41f81a4
TekMike365 <miskosklenka@gmail.com>
2025-02-09 13:57:28 +0100

    removed logging + fixed few issues

--- src/Components/Plant.h
+++ src/Components/Plant.h
@@ -18,11 +18,8 @@ struct PlantDNA
     std::array<int, ROOT_GROW_ACTION_LEN> LeafGrowAction;
 
     static const int MAX_VALUE = 100;
+    static const int MAX_GROW_CHOICE_VALUE = 10;
     static const int MIN_GROW_ACTION_VALUE = 10;
-    static const int MIN_ROOT_GROW_CHOICE_VAL = 10;
-    static const int MIN_STEM_GROW_CHOICE_VAL = 10;
-    static const int MIN_LEAF_GROW_CHOICE_VAL = 10;
-    static const int MIN_FRUIT_GROW_CHOICE_VAL = 10;
 };
 
 class Plant
@@ -108,47 +105,6 @@ private:
     inline int GetWaterBonus() const { return m_SoilResources.Potassium / GetSize() * POTASSIUM_BONUS_CONSTANT; }
     inline int GetLightBonus() const { return m_SoilResources.Phosphorus / GetSize() * PHOSPHORUS_BONUS_CONSTANT; }
     inline int GetSoilBonus() const { return m_SoilResources.Nitrogen / GetSize() * NITROGEN_BONUS_CONSTANT; }
-
-    inline void LogDNA() const
-    {
-        Log::SimTrace("[pid: {}] DNA:", m_ID);
-        Log::SimTrace("[pid: {}]     GrowChoice:", m_ID);
-        Log::SimTrace("[pid: {}]         RootGA: {}", m_ID, m_DNA.GrowthChoice[0]);
-        Log::SimTrace("[pid: {}]         StemGA: {}", m_ID, m_DNA.GrowthChoice[1]);
-        Log::SimTrace("[pid: {}]         LeafGA: {}", m_ID, m_DNA.GrowthChoice[2]);
-        Log::SimTrace("[pid: {}]         FruitGA: {}", m_ID, m_DNA.GrowthChoice[3]);
-        Log::SimTrace("[pid: {}]     RootGrowAction:", m_ID);
-        for (int i = 0; i < m_DNA.ROOT_GROW_ACTION_LEN; i++)
-            Log::SimTrace("[pid: {}]         {}: {}", m_ID, i, m_DNA.RootGrowAction[i]);
-        Log::SimTrace("[pid: {}]     LeafGrowAction:", m_ID);
-        for (int i = 0; i < m_DNA.LEAF_GROW_ACTION_LEN; i++)
-            Log::SimTrace("[pid: {}]         {}: {}", m_ID, i, m_DNA.LeafGrowAction[i]);
-    }
-
-    inline void LogPosVector(const std::vector<glm::uvec3> &vec, const std::string &msg) const
-    {
-        Log::SimTrace("[pid: {}] {}:", m_ID, msg);
-        for (int i = 0; i < vec.size(); i++)
-            Log::SimTrace("[pid: {}]     {}: [{}, {}, {}]", m_ID, i, vec[i].x, vec[i].y, vec[i].z);
-    }
-
-    inline void LogPlantResources() const
-    {
-        Log::SimTrace("[pid: {}] Resources:", m_ID);
-        Log::SimTrace("[pid: {}]     Water: {}", m_ID, m_Water);
-        Log::SimTrace("[pid: {}]     Light: {}", m_ID, m_Light);
-        Log::SimTrace("[pid: {}]     Soil Resources:", m_ID);
-        Log::SimTrace("[pid: {}]         Phosphorus: {}", m_ID, m_SoilResources.Phosphorus);
-        Log::SimTrace("[pid: {}]         Potassium: {}", m_ID, m_SoilResources.Potassium);
-        Log::SimTrace("[pid: {}]         Nitrogen: {}", m_ID, m_SoilResources.Nitrogen);
-        Log::SimTrace("[pid: {}] Survival Cost:", m_ID);
-        Log::SimTrace("[pid: {}]     Water: {}", m_ID, WATER_SURVIVE_COST_MPLR * GetSize());
-        Log::SimTrace("[pid: {}]     Light: {}", m_ID, LIGHT_SURVIVE_COST_MPLR * GetSize());
-        Log::SimTrace("[pid: {}]     Soil Resources:", m_ID);
-        Log::SimTrace("[pid: {}]         Phosphorus: {}", m_ID, NITROGEN_SURVIVE_COST_MPLR * GetSize());
-        Log::SimTrace("[pid: {}]         Potassium: {}", m_ID, PHOSPHORUS_SURVIVE_COST_MPLR * GetSize());
-        Log::SimTrace("[pid: {}]         Nitrogen: {}", m_ID, POTASSIUM_SURVIVE_COST_MPLR * GetSize());
-    }
 
 private:
     std::shared_ptr<World> m_World;
